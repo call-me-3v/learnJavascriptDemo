a68cd6f193e191eeac0bc521f11155482302e913
venividivici2003 <3vdataconsult@gmail.com>
2020-06-08 11:49:35 +0100

    commit user entity class

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-class Person {
-  constructor({ name, age }) {
-    this.name;
-    this.age;
+/* this class get user info*/
+class UserEntity {
+  constructor({ username, fullname, email, password }) {
+    this._username = username;
+    this._fullname = fullname;
+    this._email = email;
+    this._password = password;
   }
-  getPersonName() {}
+
+  getUsername() {
+    return this._username;
+  }
+
+  getFullName() {
+    return this._fullname;
+  }
+
+  getUserEmail() {
+    return this._email;
+  }
+
+  getUserPassword() {
+    return this._password;
+  }
 }
+
+// instantiate user entity
+const userEntity = new UserEntity({
+  username: "venividivici",
+  fullname: "Vincent Asamoah",
+  email: "3vdatacon",
+  password: 1234,
+});
+
+// print to console
+
+console.log(userEntity.getUsername());
+
+console.log(userEntity.getFullName());
+
+console.log(userEntity.getUserEmail());
+
+console.log(userEntity.getUserPassword());
